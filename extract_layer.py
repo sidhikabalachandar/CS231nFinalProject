f33c7a39edcda0400a5e9ac8693a2de77bb64561
Avi Goyal <sagoyal@stanford.edu>
2021-05-19 23:50:21 -0700

    adding extract_layers

---
+++ extract_layer.py
@@ -0,0 +1,33 @@
+import torch
+import numpy as np
+import open3d as o3d
+
+
+def main():    
+    
+    #Load test point cloud
+    path_to_car = "data/shape_net_core_uniform_samples_2048/02958343/627c561f2a45ac907c4509228487875f.ply"
+    point_cloud = o3d.io.read_point_cloud(path_to_car)
+    point_cloud_in_numpy = np.asarray(point_cloud.points)
+    point_cloud_in_numpy = point_cloud_in_numpy.reshape(-1)
+    
+    #Convert point cloud to tensor
+    input_tensor = torch.from_numpy(point_cloud_in_numpy)
+    
+    #find device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    #load model
+    model = torch.load('./best_496_car_model.pt')
+    model.eval()
+    
+    #Print tensors
+    x = input_tensor
+    for l in list(model.children()):
+        x = l(x)
+        print(x)
+    
+    
+
+if __name__ == "__main__":
+    main()
