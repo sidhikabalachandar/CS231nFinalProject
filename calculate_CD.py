c4ca13a2aabd23a136a82e4ead27fb610d4e8e70
sidhikabalachandar <sidhikab@stanford.edu>
2021-12-01 00:36:57 -0800

    sanity check

--- calculate_CD.py
+++ calculate_CD.py
@@ -36,7 +36,7 @@ def main():
     args = parser.parse_args()
 
     #Load Train Data
-    batch_size = 256
+    batch_size = 2
     noise_size=128
     num_points = 2048
     trainset = PointCloudDataset(path_to_data = args.train_path)
