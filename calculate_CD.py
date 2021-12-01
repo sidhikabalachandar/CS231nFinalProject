bba1bb0b3ddad773b6f45a9d6f4f6d9069a3e279
Avi Goyal <sagoyal@stanford.edu>
2021-11-30 22:27:18 -0800

    reshaping stuff

--- calculate_CD.py
+++ calculate_CD.py
@@ -12,22 +12,20 @@ from AE_models.maf import *
 
 def getCD(criterion, pc_1, pc_2):
 
-    # pc_1 & pc_2 are both of size (batch_size, num_pts, 3)
-    print(pc_1.size(), pc_2.size())
     dist1, dist2, _, _ = criterion(pc_1, pc_2)
     dist = torch.mean(torch.sum(dist1 + dist2, axis = 1))
     return dist
 
-def get_gan_data(G, ae, batch_size, noise_size):
+def get_gan_data(G, ae, batch_size, noise_size, num_points):
     g_fake_seed = sample_noise(batch_size, noise_size).type(dtype)
     fake_images = G(g_fake_seed)
-    fake_images = decode(ae, fake_images)
+    fake_images = decode(ae, fake_images).reshape((batch_size, num_points, 3))
     return fake_images
     
-def get_flow_data(model, ae, batch_size):
+def get_flow_data(model, ae, batch_size, num_points):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     fake_images = model.sample(device, n=batch_size)
-    fake_images = decode(ae, fake_images)
+    fake_images = decode(ae, fake_images).reshape((batch_size, num_points, 3))
     return fake_images
 
 def main():
@@ -42,6 +40,7 @@ def main():
     #Load Train Data
     batch_size = 256
     noise_size=128
+    num_points = 2048
     trainset = PointCloudDataset(path_to_data = args.train_path)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
 
@@ -52,9 +51,9 @@ def main():
     ae = load_ae(args.ae_name)
 
     if args.type == "gan":
-        example_fake = get_gan_data(model, ae, batch_size, noise_size)
+        example_fake = get_gan_data(model, ae, batch_size, noise_size, num_points)
     elif args.type == "maf":
-        example_fake = get_flow_data(model, ae, batch_size)
+        example_fake = get_flow_data(model, ae, batch_size, num_points)
 
     for example_real, _ in trainloader: # get first batch of real examples
         example_real = example_real.type(dtype)
@@ -62,13 +61,8 @@ def main():
         break
 
     criterion = chamfer.chamfer_3DDist()
-    
-    CD = 0
-    for i in range(batch_size):
-        for j in range(batch_size):
-            CD += getCD(criterion, example_fake[i], example_real[j])
-
-    average_CD = CD/(i*j)
+    CD = getCD(criterion, example_fake, example_real)
+    average_CD = CD/(batch_size*batch_size)
     print('Average CD: {}'.format(average_CD))
 
 if __name__ == "__main__":
